6c30f6f299727abf409aa9b8d699b77ecd71673d
Rishabh Goyal <rishabhgoyal0001@gmail.com>
2022-09-29 14:42:26 -0400

    US01 Dates before current Date

--- Team_1_Gedcom_Project.py
+++ Team_1_Gedcom_Project.py
@@ -1,20 +1,24 @@
 
+from distutils.errors import DistutilsFileError
+from sqlite3 import Date
 import sys
 import individual
 import family
 import datetime
 from prettytable import PrettyTable
 
-validTags = ["INDI", "NAME", "SEX", "BIRT", "DEAT", "FAMC", "MARR", "HUSB", 
-             "WIFE", "CHIL", "DIV", "DATE", "HEAD", "TRLR", "NOTE", "FAMS", 
+validTags = ["INDI", "NAME", "SEX", "BIRT", "DEAT", "FAMC", "MARR", "HUSB",
+             "WIFE", "CHIL", "DIV", "DATE", "HEAD", "TRLR", "NOTE", "FAMS",
              "FAM"]
 individuals = {}
 families = {}
 
 # takes in the date string used in a Gedcom file and returns a date object of that date
+
+
 def getDate(dateString):
-    monthDict = {"JAN":1, "FEB":2, "MAR":3, "APR":4, "MAY":5, "JUN":6, 
-                 "JUL":7, "AUG":8, "SEP":9, "OCT":10, "NOV":11, "DEC":12}
+    monthDict = {"JAN": 1, "FEB": 2, "MAR": 3, "APR": 4, "MAY": 5, "JUN": 6,
+                 "JUL": 7, "AUG": 8, "SEP": 9, "OCT": 10, "NOV": 11, "DEC": 12}
     date = dateString.split()
     day = int(date[0])
     month = monthDict[date[1]]
@@ -22,128 +26,165 @@ def getDate(dateString):
     return datetime.datetime(year, month, day).date()
 
 # takes in a Gedcom tag and returns if it is valid
+
+
+def isDateSmallerThanCurrentDate(date):
+    currentDate = datetime.date.today()
+    return date < currentDate
+
+
 def isTagValid(tag):
     return tag in validTags
 
 # takes in a list of Gedcom rows pertaining to an individual and returns an indvidual object
+
+
 def readIndividual(rowList):
-    newIndiv = individual.individual()
-    
-    # Booleans for tracking if we are reading in birth and death dates
-    readingBirth = False
-    readingDeath = False
-    
-    for row in rowList:
-        # if the level is 1 then that means we are no longer reading in birth or death dates
-        if int(row[0]) == 1:
-            readingBirth = False
-            readingDeath = False
-        
-        # Reads each row and sets the corresponding field in the individual object
-        if row[1] == "INDI":
-            newIndiv.identifier = row[2].strip()
-        elif row[1] == "NAME":
-            newIndiv.name = row[2].strip()
-        elif row[1] == "SEX":
-            newIndiv.gender = row[2].strip()
-        elif row[1] == "DEAT":
-            if "Y" == row[2].strip():
-                # Begin reading in the death date
-                readingDeath = True
-                newIndiv.alive = False
-            else:
-                newIndiv.alive = True
-        elif row[1] == "BIRT":
-            # Begin reading in the birth date
-            readingBirth = True
-        elif int(row[0]) == 2 and row[1] == "DATE":
-            if readingBirth:
-                newIndiv.birthday = getDate(row[2].strip())
-            elif readingDeath:
-                newIndiv.deathday = getDate(row[2].strip())
-        elif row[1] == "FAMS":
-            newIndiv.spouseFam.append(row[2].strip())
-        elif row[1] == "FAMC":
-            newIndiv.childFam.append(row[2].strip())
-    
-    newIndiv.calculateAge()
-    return newIndiv
-    
+    try:
+        newIndiv = individual.individual()
+        # Booleans for tracking if we are reading in birth and death dates
+        readingBirth = False
+        readingDeath = False
+        for row in rowList:
+            # if the level is 1 then that means we are no longer reading in birth or death dates
+            if int(row[0]) == 1:
+                readingBirth = False
+                readingDeath = False
+
+            # Reads each row and sets the corresponding field in the individual object
+            if row[1] == "INDI":
+                newIndiv.identifier = row[2].strip()
+            elif row[1] == "NAME":
+                newIndiv.name = row[2].strip()
+            elif row[1] == "SEX":
+                newIndiv.gender = row[2].strip()
+            elif row[1] == "DEAT":
+                if "Y" == row[2].strip():
+                    # Begin reading in the death date
+                    readingDeath = True
+                    newIndiv.alive = False
+                else:
+                    newIndiv.alive = True
+            elif row[1] == "BIRT":
+                # Begin reading in the birth date
+                readingBirth = True
+            elif int(row[0]) == 2 and row[1] == "DATE":
+                if readingBirth:
+                    birthDate = getDate(row[2].strip())
+                    if (isDateSmallerThanCurrentDate(birthDate) == False):
+                        raise Exception("Invalid birth date")
+                    newIndiv.birthday = birthDate
+
+                elif readingDeath:
+                    deathday = getDate(row[2].strip())
+                    if (isDateSmallerThanCurrentDate(birthDate) == False):
+                        raise Exception("Invalid death date")
+                    newIndiv.deathday = deathday
+
+            elif row[1] == "FAMS":
+                newIndiv.spouseFam.append(row[2].strip())
+            elif row[1] == "FAMC":
+                newIndiv.childFam.append(row[2].strip())
+
+        newIndiv.calculateAge()
+        return newIndiv
+    except Exception as e:
+        print(e)
+
 # takes in a list of Gedcom rows pertaining to an family and returns an family object
+
+
 def readFamily(rowList):
-    newFam = family.family()
-    
-    # Booleans for tracking if we are reading in marriage and divorce dates
-    readingMarriage = False
-    readingDivorce = False
-    
-    for row in rowList:
-        # if the level is 1 then that means we are no longer reading in marriage or divorce dates
-        if int(row[0]) == 1:
-            readingMarriage = False
-            readingDivorce = False
-        
-        # Reads each row and sets the corresponding field in the family object    
-        if row[1] == "FAM":
-            newFam.identifier = row[2].strip()
-        elif row[1] == "HUSB":
-            newFam.husbandId = row[2].strip()
-        elif row[1] == "WIFE":
-            newFam.wifeId = row[2].strip()
-        elif row[1] == "CHIL":
-            newFam.children.append(row[2].strip())
-        elif row[1] == "DIV":
-            # Begin reading in the divorce date
-            readingDivorce = True
-            newFam.isDivorced = True
-        elif row[1] == "MARR":
-            # Begin reading in the marriage date
-            readingMarriage = True
-        elif int(row[0]) == 2 and row[1] == "DATE":
-            if readingMarriage:
-                newFam.married = getDate(row[2].strip())
-            elif readingDivorce:
+
+    try:
+        newFam = family.family()
+
+        # Booleans for tracking if we are reading in marriage and divorce dates
+        readingMarriage = False
+        readingDivorce = False
+        for row in rowList:
+
+            # if the level is 1 then that means we are no longer reading in marriage or divorce dates
+            if int(row[0]) == 1:
+                readingMarriage = False
+                readingDivorce = False
+
+            # Reads each row and sets the corresponding field in the family object
+            if row[1] == "FAM":
+                newFam.identifier = row[2].strip()
+            elif row[1] == "HUSB":
+                newFam.husbandId = row[2].strip()
+            elif row[1] == "WIFE":
+                newFam.wifeId = row[2].strip()
+            elif row[1] == "CHIL":
+                newFam.children.append(row[2].strip())
+            elif row[1] == "DIV":
+                # Begin reading in the divorce date
+                readingDivorce = True
                 newFam.isDivorced = True
-                newFam.divorced = getDate(row[2].strip())
-                
-    return newFam
+            elif row[1] == "MARR":
+                # Begin reading in the marriage date
+                readingMarriage = True
+            elif int(row[0]) == 2 and row[1] == "DATE":
+                if readingMarriage:
+                    marriageDate = getDate(row[2].strip())
+                    if isDateSmallerThanCurrentDate(marriageDate): raise Exception("Invalid Marriage date")
+                    newFam.married = marriageDate
+                elif readingDivorce:
+                    newFam.isDivorced = True
+                    divorceDate = getDate(row[2].strip())
+                    if isDateSmallerThanCurrentDate(divorceDate): raise Exception("Invalid Divorce date")
+                    newFam.divorced = divorceDate
+
+        return newFam
+    except Exception as e:
+        print(e)
+
 
 # Print out the individuals and families from the Gedcom file using prettytable
+
+
 def printOutput():
     indPT = PrettyTable()
     famPT = PrettyTable()
 
-    indPT.field_names = ["ID", "NAME", "GENDER", "BIRTHDAY", "AGE", "ALIVE", "DEATH", "CHILD", "SPOUSE"]
-    famPT.field_names = ["ID", "MARRIED", "DIVORCED", "HUSBAND ID", "HUSBAND NAME", "WIFE ID", "WIFE NAME", "CHILDREN"]
+    indPT.field_names = ["ID", "NAME", "GENDER", "BIRTHDAY",
+                         "AGE", "ALIVE", "DEATH", "CHILD", "SPOUSE"]
+    famPT.field_names = ["ID", "MARRIED", "DIVORCED", "HUSBAND ID",
+                         "HUSBAND NAME", "WIFE ID", "WIFE NAME", "CHILDREN"]
 
     for individual in sorted(individuals.keys()):
         ind = individuals[individual]
-        indPT.add_row([ind.identifier, ind.name, ind.gender, ind.birthday, ind.age, ind.alive, ind.getDeathday(), ind.getChildFam(), ind.getSpouseFam()])
+        indPT.add_row([ind.identifier, ind.name, ind.gender, ind.birthday, ind.age,
+                      ind.alive, ind.getDeathday(), ind.getChildFam(), ind.getSpouseFam()])
     for family in sorted(families.keys()):
         fam = families[family]
-        famPT.add_row([fam.identifier, fam.married, fam.getIsDivorced(), fam.husbandId, fam.husbandName, fam.wifeId, fam.wifeName, fam.getChildren()])
-        
+        famPT.add_row([fam.identifier, fam.married, fam.getIsDivorced(
+        ), fam.husbandId, fam.husbandName, fam.wifeId, fam.wifeName, fam.getChildren()])
+
     print("Individuals")
     print(indPT)
     print("Families")
     print(famPT)
 
 # Process teh Gedcom file and store it
+
+
 def processGedcomFile(file):
 
     # Booleans to keep track of when we are reading a family in and when we are reading an individual
     readingIndividual = False
     readingFamily = False
-    
+
     # List for the lines that correspond to the family or individual we are reading in
     linesList = []
-    
+
     for line in file:
-        
+
         splitLine = line.split()
         # get the level for the Gedcom line
         level = splitLine.pop(0)
-        
+
         # get the tag and argument for the Gedcom line
         if len(splitLine) == 1:
             tag = splitLine[0]
@@ -157,7 +198,7 @@ def processGedcomFile(file):
             arguments = ""
             for word in splitLine:
                 arguments = arguments + word + " "
-        
+
         # Level 0 marks the start of a new family or individual
         if int(level) == 0:
             # stop reading in for the previous family or individual and add them to teh appropriate dictionary
@@ -167,7 +208,7 @@ def processGedcomFile(file):
             if readingFamily:
                 newFamily = readFamily(linesList)
                 families[newFamily.identifier] = newFamily
-            
+
             # start reading in for a new individual
             if tag == "INDI":
                 readingIndividual = True
@@ -182,14 +223,15 @@ def processGedcomFile(file):
             else:
                 readingIndividual = False
                 readingFamily = False
-        
+
         if isTagValid(tag):
             linesList.append([level, tag, arguments])
-        
+
         # Add the parents names to the families
         for family in families.keys():
             families[family].husbandName = individuals[families[family].husbandId].name
             families[family].wifeName = individuals[families[family].wifeId].name
+
 
 def main():
     if len(sys.argv) == 2:
@@ -201,7 +243,7 @@ def main():
             print("Error opening GEDCOM FILE.")
     else:
         print("Error in number of arguments. Please provide the name of one GEDCOM file.")
-    
-    
+
+
 if __name__ == "__main__":
     main()
